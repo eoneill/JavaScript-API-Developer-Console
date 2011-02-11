336b6e9cad48c88da66deaedd21aad234a6f949c
Eugene ONeill <eoneill@linkedin.com>
2011-02-10 20:28:52 -0800

    updated

--- js/console.js
+++ js/console.js
@@ -125,8 +125,8 @@ var tryLoadExample = function() {
     }
     else {
       // load a random example
-      var $category;
-      /*do {
+      /*var $category;
+      do {
         $category = $(".example-group:random", "#examples");
       } while ( !$category.is(".load-url") );*/
       $(".example-group:random", "#examples").show().find("a:random").click();  // select a random example from the open category
